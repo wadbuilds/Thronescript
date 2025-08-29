78fd24ef23b3a087971660d9cd998aeeb9f8fe20
wadbuilds <justwad20@gmail.com>
2025-08-29 09:58:03 +0000

    Add Vercel Analytics to Thronescript

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { inject } from '@vercel/analytics';
 import './index.css'
 import App from './App.jsx'
 
@@ -7,4 +8,5 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+inject();
